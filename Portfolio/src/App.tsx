b9571fec7698afe2114915578bf02a07ac31f8c0
NahualliArt <artnahualli@gmail.com>
2023-03-28 19:12:07 -0600

    assets

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import { useState} from 'react';
+import { ThemeContext } from './contexts/theme-context';
+import Header from './components/Header';
+import Home from './components/Home';
+import Construccion from './components/errors/Construccion'; //Pagina de mantenimiento
+import Footer from './components/Footer';
+import Error from './components/errors/Error';
+import './styles/App.scss';
+import { BrowserRouter, Routes, Route, useRoutes } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App(): JSX.Element {
+
+  // Detecting the default theme
+  
+  const isBrowserDefaultDark = () => window.matchMedia('(prefers-color-scheme: dark)').matches;
 
   return (
-    <>
-    </>
-  )
+      <div>
+        <BrowserRouter>
+          <Header/>
+          <Routes>
+            <Route path="" element={<Home/>}/>
+          </Routes>
+          <Construccion/>
+          <Error/>
+          <Footer/>
+        </BrowserRouter>
+      </div>
+  );
 }
-
-export default App
