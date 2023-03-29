67dd2523ca4d2c40720298a313e52c249e011d1a
NahualliArt <artnahualli@gmail.com>
2023-03-29 12:48:48 -0600

    Style errors components

--- Portfolio/src/components/errors/Construccion.tsx
+++ Portfolio/src/components/errors/Construccion.tsx
@@ -1,17 +1,48 @@
 
-import Footer from '../animations/FooterAnima';
+//construccion.tsx
+import '../../styles/App.scss'
+import styled from "styled-components";
+import footerGif from '../../assets/animations/footer-optimize.gif';
+import { useTranslation } from 'react-i18next';
+
+
+//Styles
+const Wrapper = styled.section`
+  padding: 4em;
+`;
+
+const Title = styled.h1`
+  font-size: 2.5em;
+  text-align: center;
+  color: #CDCDCD;
+`;
+
+
+const Image = styled.div`
+    background-image: url(${footerGif});
+    background-size: cover;
+    width: 400px;
+    height: 400px;
+    margin: auto;
+`;
 
 function Construccion() {
 
-    return(
- 
-        <>
-            <div>
-                <h1>Mantenimiento</h1>
-            </div>
-            <Footer/>
-        </> 
+    //Trasnlator
+    const { t, i18n } = useTranslation();
 
+    const onClickLanguageChange = (e: any) => {
+    const language = e.target.value;
+    i18n.changeLanguage(language);
+    }
+
+    return(
+        <>
+        <Wrapper>
+            <Title>{t("errors.cooming")}</Title>
+            <Image/>
+        </Wrapper>
+        </>
     );
 };
 
--- Portfolio/src/components/errors/Error.tsx
+++ Portfolio/src/components/errors/Error.tsx
@@ -1,10 +1,52 @@
+//error.tsx
+import styled from "styled-components";
+import footerGif from '../../assets/animations/footer-optimize.gif';
+import { useTranslation } from 'react-i18next';
+
+//Styles
+const Wrapper = styled.section`
+  padding: 4em;
+`;
+
+const Title = styled.h1`
+  font-size: 2.5em;
+  text-align: center;
+  color: #CDCDCD;
+`;
+
+const NameError = styled.h2`
+  font-size: 3.5em;
+  text-align: center;
+  color: #EA0000;
+`;
+
+
+const Image = styled.div`
+    background-image: url(${footerGif});
+    background-size: cover;
+    width: 400px;
+    height: 400px;
+    margin: auto;
+`;
+
 function Error(){
 
+    //Trasnlator
+    const { t, i18n } = useTranslation();
+
+    const onClickLanguageChange = (e: any) => {
+    const language = e.target.value;
+    i18n.changeLanguage(language);
+    }
+
     return(
-        <div>
-            <h1>Te perdiste</h1>
-            <h2>404 Error</h2>
-        </div>
+        <>
+        <Wrapper>
+            <NameError>404</NameError>
+            <Title>{t("errors.404")}</Title>
+            <Image/>
+        </Wrapper>
+        </>
     );
 };
 
