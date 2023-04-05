120c51c1f2ba13455530855ac809f1824d57bdb3
NahualliArt <artnahualli@gmail.com>
2023-04-04 18:26:23 -0600

    translate modal JSON

--- src/components/alerts/AterrizaSocio.tsx
+++ src/components/alerts/AterrizaSocio.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from "react";
+import { useTranslation } from 'react-i18next';
 import '../../styles/alerts.scss'
 
 interface ModalType {
@@ -8,14 +9,23 @@ interface ModalType {
 }
 
 export default function AterrizaSocio(props: ModalType) {
+
+    //translator
+    const { t, i18n } = useTranslation();
+
+    const onClickLanguageChange = (e: any) => {
+        const language = e.target.value;
+        i18n.changeLanguage(language);
+    } 
+
     return(
         <>
             {props.isOpen && (
                 <div className="overlay" onClick={props.toggle}>
                     <div className='pop-up'>
-                        <h3>Somos Parters!!</h3>
-                        <p>Trabajamos en asociación comunitatia con</p>
-                        <a>Aterrizatusideas.com</a>
+                        <h3>{t("alerts.aterriza.titulo")}</h3>
+                        <p>{t("alerts.aterriza.text")}</p>
+                        <a href="aterrizatusideas.com/" target="_blank">Aterrizatusideas.com</a>
                     </div>
                 </div>
             )}
